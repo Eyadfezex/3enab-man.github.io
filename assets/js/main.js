2f795b3ae19655f3d066a4757c38070964c078fc
Eyadfezex <112803756+Eyadfezex@users.noreply.github.com>
2023-10-01 02:22:14 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,4 @@
-/*=============== CHANGE BACKGROUND HEADER ===============*/
-// const scrollHeader = () =>{
-//     const header = document.getElementById('header')
-//     // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
-//     this.scrollY >= 50 ? header.classList.add('scroll-header') : header.classList.remove('scroll-header')
-// }
-// window.addEventListener('scroll', scrollHeader)
+
 
 /*=============== scrollreveal ===============*/
 ScrollReveal({ distance: '25px' }).reveal(".landing__content" 
