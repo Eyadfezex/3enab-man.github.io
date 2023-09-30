9119c941ce3ea68355519fe403db1e213cd77335
Eyadfezex <112803756+Eyadfezex@users.noreply.github.com>
2023-10-01 02:03:58 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,72 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+// const scrollHeader = () =>{
+//     const header = document.getElementById('header')
+//     // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+//     this.scrollY >= 50 ? header.classList.add('scroll-header') : header.classList.remove('scroll-header')
+// }
+// window.addEventListener('scroll', scrollHeader)
+
+/*=============== scrollreveal ===============*/
+ScrollReveal({ distance: '25px' }).reveal(".landing__content" 
+,{delay:300,duration:1500,easing:"ease",origin:"bottom" });
+
+ScrollReveal({ distance: '35px' }).reveal(".landing__subtitle" 
+,{delay:300,duration:1500,easing:"ease",origin:"top" });
+
+
+ScrollReveal({ distance: '35px' }).reveal("#c_1" 
+,{delay:180,duration:1000,easing:"ease",origin:"bottom"});
+
+ScrollReveal({ distance: '25px' }).reveal("#c_2" 
+    ,{delay:100,
+    duration:1000,
+    easing:"ease",
+    origin:"bottom"});
+
+ScrollReveal({ distance: '35px' }).reveal("#c_3" 
+    ,{delay:180,
+    duration:1000,
+    easing:"ease",
+    origin:"bottom" });
+
+ScrollReveal({ distance: '35px' }).reveal(".cards-c",{delay:300,
+    duration:1000,
+    easing:"ease",
+    origin:"bottom"});
+
+ScrollReveal({ distance: '35px' }).reveal(".characters__title",{delay:150,
+    duration:1000,
+    easing:"ease",
+    origin:"top"});
+
+    ScrollReveal({ distance: '35px' }).reveal(".cutscenes__subtitle",{delay:150,
+        duration:1000,
+        easing:"ease",
+        origin:"top"});
+
+        ScrollReveal({ distance: '20px' }).reveal("#v-1",{delay:300,
+            duration:1000,
+            easing:"ease",
+            origin:"bottom"});
+
+            ScrollReveal({ distance: '20px' }).reveal("#v-2",{delay:300,
+                duration:1000,
+                easing:"ease",
+                origin:"bottom"});
+
+                ScrollReveal({ distance: '20px' }).reveal("#v-3",{delay:300,
+                    duration:1000,
+                    easing:"ease",
+                    origin:"bottom"});
+
+                    ScrollReveal({ distance: '20px' }).reveal("#v-4",{delay:300,
+                        duration:1000,
+                        easing:"ease",
+                        origin:"bottom"});
+
+                        ScrollReveal({ distance: '20px' }).reveal("#v-5",{delay:300,
+                            duration:1000,
+                            easing:"ease",
+                            origin:"bottom"});
+                        
+
